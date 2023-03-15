0ae2060b4927d13e471b27529207f9165514739d
AleAlle62 <basketallegrini@gmail.com>
2023-03-15 15:44:22 +0100

    no bonus

--- CibiPreferiti/Program.cs
+++ CibiPreferiti/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] preferiti = { "Pasta", "pizza", "tacos", "hamburger", "sushi" };
+Console.WriteLine("la lunghezza della classifica e:" + preferiti.Length);
+
+Console.WriteLine("la classifica:");
+for (int i = 0; i < preferiti.Length; i++)
+{
+    Console.WriteLine((i+1) + ":" + preferiti[i]);
+}
+
+Console.WriteLine("il mio cibo:" + preferiti[0]);
+Console.WriteLine("meno preferito:" + preferiti[preferiti.Length-1]);
